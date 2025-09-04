ed5bab07f03663df521d1e4933f7f2795faa4e11
Elle Simonds <ellesimonds@gmail.com>
2025-09-04 11:10:57 -0500

    Added info.txt

--- app/src/main/java/com/example/repairorder/MainActivity.java
+++ app/src/main/java/com/example/repairorder/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.repairorder;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    double numbers = 0.0;
+
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView totalTextView = findViewById(R.id.totalCalculation);
+
+        Random gen = new Random();
+
+        double number = gen.nextDouble();
+        String v1 = "$"+number;
+        totalTextView.setText(v1);
+
+        // this is to set the name; done also in hardcode; totalTextView.setText(R.string.dollarempty);
+        String value = totalTextView.getText().toString();
+
+
+        Log.i("TEST", value);
+
+
+
     }
+
 }
