1da63a9676416ebba760bab59ada52de3a0c8b68
Elle Simonds <ellesimonds@gmail.com>
2025-09-04 11:56:30 -0500

    Added info.txt

--- app/src/main/java/com/example/repairorder/MainActivity.java
+++ app/src/main/java/com/example/repairorder/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.repairorder;
 
+//import static kotlin.text.ScreenFloatValueRegEx.value;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,36 +21,51 @@ public class MainActivity extends AppCompatActivity {
 
     double numbers = 0.0;
 
-    @Override
+    TextView totalTextView;
+    TextView subtotalTextView;
+    EditText orderInputTV;
+    TextView paintInputTV;
+
+    Button submitB;
 
 
-    protected void onCreate(Bundle savedInstanceState) {
+    //create listener
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Random gen = new Random();
+
+            double number = gen.nextDouble();
+            String v1 = "$" + number;
+            totalTextView.setText(v1);
+
+            // this is to set the name; done also in hardcode; totalTextView.setText(R.string.dollarempty);
+            String value = totalTextView.getText().toString();
+
+            String orderTypeValue = orderInputTV.getText().toString();
+
+            //String
+        }
+    };
 
 
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        TextView totalTextView = findViewById(R.id.totalCalculation);
+        protected void onCreate(Bundle savedInstanceState) {
 
-        Random gen = new Random();
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_main);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
 
-        double number = gen.nextDouble();
-        String v1 = "$"+number;
-        totalTextView.setText(v1);
-
-        // this is to set the name; done also in hardcode; totalTextView.setText(R.string.dollarempty);
-        String value = totalTextView.getText().toString();
-
-
-        Log.i("TEST", value);
-
-
-
-    }
-
+            subtotalTextView = findViewById(R.id.subtotalCalculation);
+            totalTextView = findViewById(R.id.totalCalculation);
+            submitB = findViewById(R.id.submitButton);
+            submitB.setOnClickListener(buttonListener);
+            orderInputTV = findViewById(R.id.ordertypeInput);
+            paintInputTV = findViewById(R.id.paintInput);
+            //Log.i("TEST", String.valueOf(value));
+        }
 }
